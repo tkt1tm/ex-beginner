e2c781c089115979ce46b3155b05e53116758077
tkt1tm <takuto.itami.l@gmail.com>
2025-05-19 15:16:47 +0900

    合計金額を表示するメソッドの作成

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +13,16 @@ public class Exam03Controller {
     @Autowired
     private ServletContext application;
 
-    @GetMapping("input")
+    @GetMapping("/input")
     public String input() {
         return "exam03";
+    }
+
+    @PostMapping("/result")
+    public String result(int item1, int item2, int item3) {
+        int total = item1 + item2 + item3;
+        application.setAttribute("taxExcluded", total);
+        application.setAttribute("taxIncluded", (int)(total * 1.1));
+        return "exam03-result";
     }
 }
