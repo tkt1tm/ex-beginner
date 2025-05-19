a1b125af7ce6442bc6fc177b8124aceb94141974
tkt1tm <takuto.itami.l@gmail.com>
2025-05-19 15:09:01 +0900

    入力ページを表示するメソッドの作成

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,9 +1,19 @@
 package com.example.controller;
 
+import jakarta.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/exam3")
 public class Exam03Controller {
+    @Autowired
+    private ServletContext application;
+
+    @GetMapping("input")
+    public String input() {
+        return "exam03";
+    }
 }
